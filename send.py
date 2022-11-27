332c06642af8756493c19569eca61748d1161d7e
Polina Minina <35624330+pminina01@users.noreply.github.com>
2022-11-28 00:00:30 +0300

    Update send.py

--- send.py
+++ send.py
@@ -1,13 +1,13 @@
 import os
-
 import pika
 
-credentials = pika.PlainCredentials(os.getenv("USER"), os.getenv("PASSWORD"))
-parameters = pika.ConnectionParameters(host=os.getenv('HOST'), credentials=credentials)
-connection = pika.BlockingConnection(parameters)
+def producer(user, password, host):
+    credentials = pika.PlainCredentials(user, password)
+    parameters = pika.ConnectionParameters(host=host, credentials=credentials)
+    connection = pika.BlockingConnection(parameters)
 
-channel = connection.channel()
-channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print("Message sent to queue")
-connection.close()
+    channel = connection.channel()
+    channel.queue_declare(queue='hello')
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    print("Message sent to queue")
+    connection.close()
